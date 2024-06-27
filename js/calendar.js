e650ed7a14a2f14099891b62d3efaf138898a546
FrontMD <web@mockup.digital>
2024-06-27 17:08:43 +0300

    render cards

--- js/calendar.js
+++ js/calendar.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     tabsInit()
     sidebarController()
     filterSpoilersController()
+    renderCards()
 })
 
 // Вкладки
@@ -53,7 +54,7 @@ function sidebarController() {
 
 }
 
-// Спойлеры а фильтрах
+// Спойлеры в фильтрах
 function filterSpoilersController() {
     const filterParams = document.querySelectorAll('[data-js="filterParam"]')
     
@@ -79,4 +80,159 @@ function filterSpoilersController() {
 
     })
 
+}
+
+function renderCards() {
+    const cardsWrap = document.querySelector('[data-js="cardsWrap"]');
+
+    if(!cardsWrap) return
+
+    calendarData.forEach(yearItem => {
+        //Год
+        let yearBlock = document.createElement('div')
+        yearBlock.classList.add('cards__year', 'year');
+        yearBlock.innerHTML = `<div class="year__title">${yearItem.year}</div>`
+
+        yearItem.months.forEach(monthItem => {
+            //Месяц
+            let monthBlock = document.createElement('div')
+            monthBlock.classList.add('cards__month', 'month');
+            monthBlock.innerHTML = `<div class="month__title">${monthItem.month}</div>`
+
+            //Таблица месяца
+            let monthTable = document.createElement('div')
+            monthTable.classList.add('month__table', 'cards-table');
+
+            //Заполнение таблицы месяца
+            let eventsList = monthItem.events
+
+            eventsList.sort(function(a, b) {
+                return parseFloat(a.day) - parseFloat(b.day);
+            });
+
+            eventsList.forEach(eventItem => {
+                let eventCard = document.createElement('div')
+
+                eventCard.classList.add('cards-table__card', 'cards-card');
+
+                eventCard.innerHTML = `<div class="cards-card__header">
+                                            <div class="cards-card__date">${eventItem.day + "." + paramsVariables.months[monthItem.month.toLowerCase()]}</div>
+                                            <div class="cards-card__logo">
+                                                <img src="img/calendar/brands/starHit.svg" alt="brand logo">
+                                            </div>
+                                        </div>
+                                        <div class="cards-card__title">Marie Claire x Gloria Jeans Wellness Day</div>
+                                        <div class="cards-card__tags">
+                                            <div class="cards-card__tag">Spirit</div>
+                                            <div class="cards-card__tag">Print/Digital</div>
+                                            <div class="cards-card__tag">Имиджевая</div>
+                                            <div class="cards-card__tag">100—500</div>
+                                            <div class="cards-card__tag">спонсоры</div>
+                                        </div>`
+                
+                monthTable.appendChild(eventCard)
+            })
+           
+
+
+            monthBlock.appendChild(monthTable)
+            yearBlock.appendChild(monthBlock)
+            cardsWrap.appendChild(yearBlock)
+        })
+
+        console.log(yearBlock)
+    })
+}
+
+const calendarData = [
+    {
+        "year": "2019",
+        "months": [
+            {
+                "month": "февраль",
+                "events": [
+                    {
+                        "id": "Конференция Территория свободной мысли",
+                        "name": "Конференция Территория свободной мысли",
+                        "day": "28",
+                        "brand": "Антенна телесемь",
+                        "cluster": "Spirit",
+                        "department": "PRINT/DIGITAL",
+                        "direction": "ИМИДЖЕВАЯ",
+                        "number": 300,
+                        "sponsors": true
+                    },
+                    {
+                        "id": "Конференция Территория свободной мысли",
+                        "name": "Конференция Территория свободной мысли",
+                        "day": "21",
+                        "brand": "Антенна телесемь",
+                        "cluster": "Spirit",
+                        "department": "PRINT/DIGITAL",
+                        "direction": "ИМИДЖЕВАЯ",
+                        "number": 300,
+                        "sponsors": true
+                    }
+                ]
+                
+            },
+            {
+                "month": "Октябрь",
+                "events": [
+                    {
+                        "id": "Marie Claire x Gloria Jeans Wellness Day",
+                        "name": "Marie Claire x Gloria Jeans Wellness Day",
+                        "day": "10",
+                        "brand": "starHit",
+                        "cluster": "Spirit",
+                        "department": "PRINT/DIGITAL",
+                        "direction": "ИМИДЖЕВАЯ",
+                        "number": 300,
+                        "sponsors": true
+                    }
+                ]
+                
+            }  
+        ]
+    },
+    {
+        "year": "2020",
+        "months": [
+            {
+                "month": "Ноябрь",
+                "events": [
+                    {
+                        "id": "Бизнес-завтрак",
+                        "name": "Бизнес-завтрак",
+                        "day": "15",
+                        "brand": "Psychologies",
+                        "cluster": "Spirit",
+                        "department": "PRINT/DIGITAL",
+                        "direction": "ИМИДЖЕВАЯ",
+                        "number": 300,
+                        "sponsors": true
+                    }
+                ]
+                
+            }  
+        ]
+    },
+
+]
+
+const paramsVariables = {
+    months: {
+        "январь": "01",
+        "февраль": "02",
+        "март": "03",
+        "апрель": "04",
+        "май": "05",
+        "июнь": "06",
+        "июль": "07",
+        "август": "08",
+        "сентябрь": "09",
+        "октябрь": "10",
+        "ноябрь": "11",
+        "декабрь": "12",
+    }
 }
